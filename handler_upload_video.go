5f5ea5e55d8498ed68a38c277ac49602d5341789
Danilo Galisteu <danilogalisteu@gmail.com>
2025-05-03 11:35:25 -0300

    moved ffmpeg video code to separate file

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -8,8 +8,6 @@ import (
 	"mime"
 	"net/http"
 	"os"
-	"os/exec"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -17,31 +15,6 @@ import (
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
-
-func getVideoAspectRatio(filePath string) (string, error) {
-	args := fmt.Sprintf("-v error -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=noprint_wrappers=1 %s", filePath)
-	cmd := exec.Command("ffprobe", strings.Split(args, " ")...)
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to get video aspect ratio: %w", err)
-	}
-	outputStr := string(output)
-	if !strings.Contains(outputStr, "display_aspect_ratio=") {
-		return "", fmt.Errorf("failed to parse aspect ratio from output: %s", outputStr)
-	}
-
-	aspectRatio := strings.TrimSpace(strings.Split(outputStr, "=")[1])
-	switch aspectRatio {
-	case "":
-		return "", fmt.Errorf("aspect ratio is empty")
-	case "16:9":
-		return "landscape", nil
-	case "9:16":
-		return "portrait", nil
-	default:
-		return "other", nil
-	}
-}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
---
+++ video.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	args := fmt.Sprintf("-v error -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=noprint_wrappers=1 %s", filePath)
+	cmd := exec.Command("ffprobe", strings.Split(args, " ")...)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to get video aspect ratio: %w", err)
+	}
+	outputStr := string(output)
+	if !strings.Contains(outputStr, "display_aspect_ratio=") {
+		return "", fmt.Errorf("failed to parse aspect ratio from output: %s", outputStr)
+	}
+
+	aspectRatio := strings.TrimSpace(strings.Split(outputStr, "=")[1])
+	switch aspectRatio {
+	case "":
+		return "", fmt.Errorf("aspect ratio is empty")
+	case "16:9":
+		return "landscape", nil
+	case "9:16":
+		return "portrait", nil
+	default:
+		return "other", nil
+	}
+}
